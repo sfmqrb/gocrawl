949fdbc120093030f7cbd4488dd25342aa50b709
Sajad F. Maghrebi <sajad.faghfoor@gmail.com>
2022-09-24 02:29:46 +0330

    better logging

--- test/find_prime_number.go
+++ test/find_prime_number.go
@@ -4,6 +4,9 @@ package main
 
 import "fmt"
 
+var counter int
+var filterMap = make(map[chan int]int)
+
 // Send the sequence 2, 3, 4, ... to channel 'ch'.
 func Generate(ch chan<- int) {
 	for i := 2; ; i++ {
@@ -14,7 +17,8 @@ func Generate(ch chan<- int) {
 
 // Copy the values from channel 'in' to channel 'out',
 // removing those divisible by 'prime'.
-func Filter(in <-chan int, out chan<- int, prime int) {
+func Filter(in chan int, out chan int, prime int) {
+	fmt.Println("channels: ", filterMap[in], filterMap[out])
 	for {
 		i := <-in // Receive value from 'in'.
 		if i%prime != 0 {
@@ -25,15 +29,17 @@ func Filter(in <-chan int, out chan<- int, prime int) {
 
 // The prime sieve: Daisy-chain Filter processes.
 func main() {
+	counter = 0
 	ch := make(chan int) // Create a new channel.
+	filterMap[ch] = counter
+	counter++
 	go Generate(ch)      // Launch Generate goroutine.
 	for i := 0; i < 10; i++ {
 		prime := <-ch
 		fmt.Println(prime)
 		ch1 := make(chan int)
-
-		fmt.Println("here")
-
+		filterMap[ch1] = counter
+		counter++
 		go Filter(ch, ch1, prime)
 		ch = ch1
 	}
